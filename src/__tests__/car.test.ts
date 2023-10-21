445aab351225cd7ee48066be55fb40a0fa2f7d97
forky <trulyforky@gmail.com>
2023-10-22 00:03:30 +0900

    feat: 자동차 이름 길이 예외 처리

--- src/__tests__/car.test.ts
+++ src/__tests__/car.test.ts
@@ -4,4 +4,9 @@ describe('자동차는', () => {
     test('이름을 가진다', () => {
         expect(new Car("forky").name).toBe("forky");
     });
+
+    test('5글자 초과하는 이름을 가질 수 없다', () => {
+        expect(() => new Car("123456"))
+            .toThrowError("이름은 5글자를 초과할 수 없어요");
+    });
 });
--- src/main/car.ts
+++ src/main/car.ts
@@ -2,6 +2,9 @@ export class Car {
     readonly name: string;
 
     constructor(name: string) {
+        if (name.length > 5) {
+            throw new Error("이름은 5글자를 초과할 수 없어요");
+        }
         this.name = name;
     }
 }
